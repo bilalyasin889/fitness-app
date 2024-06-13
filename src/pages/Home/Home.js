99f6468a9e56cfdde61787046411e6cf56bab4ef
Bilal Yasin <byasin@tier2consulting.com>
2024-06-13 17:22:41 +0100

    M<oved styling to directory for related component.

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -9,15 +9,17 @@ import BannerImage from "../../assets/images/Banner.jpg"
 const Home = () => {
     return (
         <Box mb="50px" className="home-wrapper">
-            <Box sx={{mt: '30px'}} position="relative" p="20px" >
-                <img style={{objectFit: 'contain'}} src={BannerImage} alt="banner"/>
+            <Box sx={{mt: '30px'}} position="relative" p="20px">
+                <div className="image-container">
+                    <img style={{objectFit: 'contain'}} src={BannerImage} alt="banner"/>
+                    <h1 className="image-text">
+                        Plan your next workout here
+                    </h1>
+                </div>
             </Box>
             <Box position="relative" pl="25px" pr="25px" mt="25px">
-                <h1 className="page-tile">
-                    Plan your next workout here
-                </h1>
                 <p>
-                    Explore a wide range of exercise to try out in the gym or at home. <br/>
+                Explore a wide range of exercise to try out in the gym or at home. <br/>
                     Whether you're a newbie or a fitness buff, we have exercises for all levels, with easy step by step
                     guides.
                 </p>
